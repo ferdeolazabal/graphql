d727e105105493c3a04a2d6e04a92868928aace1
ferdeolazabal <ferdeolazabal@gmail.com>
2024-08-01 23:56:27 -0300

    Separate schmas in single files

--- server.js
+++ server.js
@@ -1,98 +1,65 @@
+const path = require("path");
 const express = require("express");
-const { createYoga, createSchema } = require("graphql-yoga");
 
-const schema = createSchema({
-  typeDefs: `
-    type Query {
-      product: Product
-      products: [Product]
-      orders: [Order]
-    }
-    type Product {
-      id: ID!
-      description: String!
-      reviews: [Review]
-      price: Float!
-    }
+const { createYoga } = require("graphql-yoga");
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 
-    type Review {
-      rating: Int!
-      comment: String
-    }
-
-    type Order {
-      id: ID!
-      date: String!
-      subtotal: Float!
-      items: [OrderItem]
-    }
-
-    type OrderItem {
-      product: Product!
-      quantity: Int!
-    }
-  `,
-
+const executableSchema = makeExecutableSchema({
+  typeDefs: loadFilesSync(path.join(__dirname, "**/*.graphql")),
   resolvers: {
     Query: {
-      product: () => ({
-        id: "1",
-        description: "T-Shirt",
-        reviews: [
+      products: () => {
+        return [
           {
-            rating: 4,
-            comment: "Great product!",
-          },
-        ],
-        price: 23.66,
-      }),
-      products: () => [
-        {
-          id: "1",
-          description: "Jacket",
-          reviews: [
-            {
-              rating: 4,
-              comment: "Great product!",
-            },
-          ],
-          price: 50.2,
-        },
-        {
-          id: "2",
-          description: "Shoes",
-          reviews: [
-            {
-              rating: 5,
-              comment: "Awesome product!",
-            },
-          ],
-          price: 20.3,
-        },
-      ],
-      orders: () => [
-        {
-          id: "1",
-          date: "2020-01-01",
-          subtotal: 100.0,
-          items: [
-            {
-              product: {
-                id: "1",
-                description: "T-Shirt",
-                reviews: [
-                  {
-                    rating: 4,
-                    comment: "Great product!",
-                  },
-                ],
-                price: 23.66,
+            id: "1",
+            description: "Jacket",
+            reviews: [
+              {
+                rating: 4,
+                comment: "Great product!",
               },
-              quantity: 2,
-            },
-          ],
-        },
-      ],
+            ],
+            price: 50.2,
+          },
+          {
+            id: "2",
+            description: "Shoes",
+            reviews: [
+              {
+                rating: 5,
+                comment: "Awesome product!",
+              },
+            ],
+            price: 20.3,
+          },
+        ];
+      },
+      orders: () => {
+        return [
+          {
+            id: "1",
+            date: "2020-01-01",
+            subtotal: 100.0,
+            items: [
+              {
+                product: {
+                  id: "1",
+                  description: "T-Shirt",
+                  reviews: [
+                    {
+                      rating: 4,
+                      comment: "Great product!",
+                    },
+                  ],
+                  price: 23.66,
+                },
+                quantity: 2,
+              },
+            ],
+          },
+        ];
+      },
     },
   },
 });
@@ -100,7 +67,7 @@ const schema = createSchema({
 const app = express();
 
 const yoga = createYoga({
-  schema,
+  schema: executableSchema,
   context: (req) => ({
     // Context factory gets called for every request
     //myToken: req.headers.get('authorization')
