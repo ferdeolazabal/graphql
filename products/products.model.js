6fdd89a095e1bf9a765f627a074a767f2135acf1
ferdeolazabal <ferdeolazabal@gmail.com>
2024-08-04 17:29:21 -0300

    Add mutations & query methods to product model

--- products/products.model.js
+++ products/products.model.js
@@ -23,6 +23,38 @@ const products = [
   },
 ];
 
+const getProductsByPrice = (min, max) => {
+  return products.filter(
+    (product) => product.price >= min && product.price <= max
+  );
+};
+
+const getProductById = (id) => {
+  return products.find((product) => product.id === id);
+};
+
+const addNewProduct = (id, description, price) => {
+  const newProduct = {
+    id,
+    description,
+    price,
+    reviews: [],
+  };
+  products.push(newProduct);
+  return newProduct;
+};
+
+const addReview = (id, rating, comment) => {
+  const product = getProductById(id);
+  product?.reviews.push({ rating, comment });
+  return product;
+};
+
 module.exports = {
   getAllProducts: () => products,
+  getProductsByPrice: (min, max) => getProductsByPrice(min, max),
+  getProductById: (id) => getProductById(id),
+  addNewProduct: (id, description, price) =>
+    addNewProduct(id, description, price),
+  addReview: (id, rating, comment) => addReview(id, rating, comment),
 };
--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -1,7 +1,26 @@
-const { getAllProducts } = require("./products.model");
+const {
+  getAllProducts,
+  getProductsByPrice,
+  getProductById,
+  addNewProduct,
+  addReview,
+} = require("./products.model");
 
 module.exports = {
   Query: {
     products: () => getAllProducts(),
+    productsByPrice: (_, args) => getProductsByPrice(args.min, args.max),
+    product: (_, args) => getProductById(args.id),
+  },
+
+  Mutation: {
+    addNewProduct: (_, args) => {
+      const { id, description, price } = args;
+      return addNewProduct(id, description, price);
+    },
+    addReview: (_, args) => {
+      const { id, rating, comment } = args;
+      return addReview(id, rating, comment);
+    },
   },
 };
